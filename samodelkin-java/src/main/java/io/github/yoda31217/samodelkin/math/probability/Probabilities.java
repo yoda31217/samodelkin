c683df338b411eaae41f69d9d8c32b741b34ecf6
yoda31217 <jbourne.nik@gmail.com>
2023-01-08 04:19:38 +0200

    Corner cases added to Probabilities class and tests

--- samodelkin-java/src/main/java/io/github/yoda31217/samodelkin/math/probability/Probabilities.java
+++ samodelkin-java/src/main/java/io/github/yoda31217/samodelkin/math/probability/Probabilities.java
@@ -1,6 +1,9 @@
 package io.github.yoda31217.samodelkin.math.probability;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static io.github.yoda31217.samodelkin.math.error.DoubleWithError.newDoubleWithError;
+import static java.lang.Double.isFinite;
+import static java.lang.Double.isNaN;
 import static org.apache.commons.math3.stat.interval.IntervalUtils.getNormalApproximationInterval;
 
 import io.github.yoda31217.samodelkin.math.error.DoubleWithError;
@@ -11,15 +14,23 @@ public class Probabilities {
   public static DoubleWithError calculateProbabilityWithError(
     int trialsCount, int successesCount, double confidenceLevel
   ) {
-    double probability = calculateProbability(trialsCount, successesCount);
     double error = calculateError(trialsCount, successesCount, confidenceLevel);
+    double probability = calculateProbability(trialsCount, successesCount);
     return newDoubleWithError(probability, error);
   }
 
   private static double calculateError(int trialsCount, int successesCount, double confidenceLevel) {
-    if (successesCount == 0) {
-      return 0.0;
-    }
+    checkArgument(trialsCount > 0,
+      "Failed to calculate probability error. Trials count %s should be positive integer.",
+      trialsCount);
+
+    checkArgument(successesCount > 0 && successesCount < trialsCount,
+      "Failed to calculate probability error. Successes count %s should be positive integer less than trials.",
+      successesCount);
+
+    checkArgument(isFinite(confidenceLevel) && !isNaN(confidenceLevel) && confidenceLevel > 0 && confidenceLevel < 1,
+      "Failed to calculate probability error. Confidence level %s should be (0;1) double.",
+      confidenceLevel);
 
     ConfidenceInterval confidenceInterval =
       getNormalApproximationInterval(trialsCount, successesCount, confidenceLevel);
--- samodelkin-java/src/test/java/io/github/yoda31217/samodelkin/math/probability/ProbabilitiesTest.java
+++ samodelkin-java/src/test/java/io/github/yoda31217/samodelkin/math/probability/ProbabilitiesTest.java
@@ -1,11 +1,20 @@
 package io.github.yoda31217.samodelkin.math.probability;
 
 import static io.github.yoda31217.samodelkin.math.probability.Probabilities.calculateProbabilityWithError;
+import static java.lang.Double.NEGATIVE_INFINITY;
+import static java.lang.Double.NaN;
+import static java.lang.Double.POSITIVE_INFINITY;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.data.Offset.offset;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import io.github.yoda31217.samodelkin.math.error.DoubleWithError;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ProbabilitiesTest {
 
@@ -18,11 +27,30 @@ class ProbabilitiesTest {
     assertThat(probability.getError()).isCloseTo(0.058798919, offset(0.0000001));
   }
 
-  @Test
-  void calculateProbabilityWithError_on0SuccessesCount_return0ProbabilityWith0Error() {
-    DoubleWithError probability = calculateProbabilityWithError(100, 0, 0.95);
+  @ParameterizedTest
+  @MethodSource("calculateProbabilityWithError_onIncorrectValues_throwEx_provider")
+  void calculateProbabilityWithError_onIncorrectValues_throwEx(
+    int trialsCount, int successesCount, double confidenceLevel, String badNumberStr
+  ) {
+    assertThatThrownBy(() -> calculateProbabilityWithError(trialsCount, successesCount, confidenceLevel))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessageContaining("Failed to calculate probability error")
+      .hasMessageContaining(badNumberStr);
+  }
 
-    assertThat(probability.getValue()).isCloseTo(0.0, offset(0.0000001));
-    assertThat(probability.getError()).isCloseTo(0.0, offset(0.0000001));
+  static Stream<Arguments> calculateProbabilityWithError_onIncorrectValues_throwEx_provider() {
+    return Stream.of(
+      arguments(100, 0, 0.95, "0"),
+      arguments(100, -1, 0.95, "-1"),
+      arguments(0, 1, 0.95, "0"),
+      arguments(-100, 1, 0.95, "-100"),
+      arguments(100, 100, 0.95, "100"),
+      arguments(100, 1, -0.95, "-0.95"),
+      arguments(100, 1, 0.0, "0.0"),
+      arguments(100, 1, 1.0, "1.0"),
+      arguments(100, 1, NaN, "NaN"),
+      arguments(100, 1, POSITIVE_INFINITY, "Infinity"),
+      arguments(100, 1, NEGATIVE_INFINITY, "-Infinity")
+    );
   }
 }
